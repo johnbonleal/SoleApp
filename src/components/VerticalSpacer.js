5af8b4231e3b9b518ff829e266c8d0861d8b7c4d
John Leal <johnbon.leal@outlook.com>
2018-11-28 17:42:52 +0800

    - added new component: VerticalSpacer

---
+++ src/components/VerticalSpacer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const VerticalSpacer = props => (
+    <View style={{height: props.height, width: '100%'}} />
+);
+
+export default VerticalSpacer;
