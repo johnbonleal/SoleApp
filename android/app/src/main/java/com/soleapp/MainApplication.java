63ace16e4850c69b36f6960009cbd6614429ba78
Unknown <johnbon.leal17@gmail.com>
2019-02-17 22:08:36 +0800

    - added new dependency: react-native-spinkit

--- android/app/src/main/java/com/soleapp/MainApplication.java
+++ android/app/src/main/java/com/soleapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.soleapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.react.rnspinkit.RNSpinkitPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSpinkitPackage(),
             new SplashScreenReactPackage(),
             new RNCameraPackage(),
             new MapsPackage(),
